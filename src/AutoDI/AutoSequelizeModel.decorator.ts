ff245e4502bebcce896fa403027af33810452b29
ShinCurry <shincurryyang@gmail.com>
2023-03-19 22:30:51 +0800

    fix AutoSequelizeModel

--- src/AutoDI/AutoSequelizeModel.decorator.ts
+++ src/AutoDI/AutoSequelizeModel.decorator.ts
@@ -1,29 +1,42 @@
 import { SequelizeModule } from "@nestjs/sequelize";
-import { DEFAULT_CONNECTION_NAME } from "@nestjs/sequelize/dist/sequelize.constants";
 import { globSync } from 'glob';
 import { chain } from "lodash";
-import { Model } from "sequelize-typescript";
 
 export type AutoSequelizeModelOptions = {
   connection?: string;
   path: string[];
+  export?: boolean;
 }
+
+/**
+ * https://github.com/sequelize/sequelize-typescript/blob/0e43d08677db3c9a94d11639315676c6b8d90388/src/model/shared/model-service.ts#L6
+ */
+const SEQUELIZE_MODEL_NAME_KEY = 'sequelize:modelName';
 
 export function AutoSequelizeModel(options: AutoSequelizeModelOptions): ClassDecorator {
   return (target: Function) => {
-    const name = options.connection || DEFAULT_CONNECTION_NAME;
-
+    const name = options.connection;
     const models: any[] = chain(options.path)
       .map((i) => globSync(i))
       .flatten()
       .map((path) => require(path))
-      .map((i: any): any[] => {
-        return Object.values(i) as any[]
-      })
+      .map((i: any): any[] => Object.values(i) as any[])
       .flatten()
-      .filter((i) => typeof i === 'function' && Model.isPrototypeOf(i))
+      .filter((i) => typeof i === 'function' && Reflect.hasMetadata(SEQUELIZE_MODEL_NAME_KEY, i.prototype))
       .value();
 
-    SequelizeModule.forFeature(models, name);
+    const module = SequelizeModule.forFeature(models, name);
+
+    Reflect.defineMetadata("imports", [
+      ...(Reflect.getMetadata("imports", target) || []),
+      module,
+    ], target);
+
+    if (options.export) {
+      Reflect.defineMetadata("exports", [
+        ...(Reflect.getMetadata("exports", target) || []),
+        module.module,
+      ], target);
+    }
   };
 }
